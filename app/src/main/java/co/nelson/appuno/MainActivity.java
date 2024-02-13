96be54a6f10a308beff14864cea69beb244e61af
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-02-13 08:50:29 -0500

    Ciclo de vida - métodos

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
@@ -39,9 +41,42 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    public double calcularArea(int l1,int l2){
-        return l1*l2;
+    /* Ciclo de vida
+     *   OnCreate(); => Siempre se ejecuta por defecto cuando se crea la aplicacion
+     *   OnStart();  => Se ejecuta cuando se abre la aplicación
+     *   OnRestart(); =>
+     *   OnResume(); => Cuando se abre desde segundo plano
+     *   OnPause(); => Cuando la aplicación está en segundo plano
+     *   OnStop(); => Actividad no visible para el usuario
+     *   OnDestroy(); => Cuando se destruye la aplicacion
+     * */
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected  void onRestart(){
+        super.onRestart();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
     }
 
+    @Override
+    protected  void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+    }
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+    }
+
+
 }
+
