d96a49916889e43896815d379f7ed3a18b69b035
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-02-13 09:25:57 -0500

    Stable version
    Emulated stable version with life loop

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,6 +3,7 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
    Button btn1,btn2;
+   String Tag = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,28 +55,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(Tag, "Method OnStart");
     }
     @Override
     protected  void onRestart(){
         super.onRestart();
+        Log.i(Tag, "Method OnRestart");
     }
     @Override
     protected void onResume(){
         super.onResume();
+        Log.i(Tag, "Method OnResume");
     }
 
     @Override
     protected  void onPause(){
         super.onPause();
+        Log.i(Tag, "Method OnPause");
     }
 
     @Override
     protected  void onStop(){
         super.onStop();
+        Log.i(Tag, "Method OnStop");
     }
     @Override
     protected  void onDestroy(){
         super.onDestroy();
+        Log.i(Tag, "Method OnDestroy");
     }
 
 
