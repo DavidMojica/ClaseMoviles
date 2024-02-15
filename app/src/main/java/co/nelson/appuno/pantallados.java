8b6235695e523c6023f7e8e5df41d0f3cc878096
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-02-15 09:29:52 -0500

    15 feb class

--- app/src/main/java/co/nelson/appuno/pantallados.java
+++ app/src/main/java/co/nelson/appuno/pantallados.java
@@ -9,16 +9,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class pantallados extends AppCompatActivity{
+    TextView usuarioDos, textoAnterior;
+    String name, text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallados);
+        usuarioDos = findViewById(R.id.usuarioDos);
+        textoAnterior = findViewById(R.findViewById.textoAnterior);
+        //tarea: recibir la informacion en cualquier otro lado.
+        //RECIBIR LOS DATOS DE LA PANTALLA EMISORA//
+        name = getIntent().getStringExtra("name");
+        text = getIntent().getStringExtra("text");
+    
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(Tag, "Method OnStart pantallados");
+        usuarioDos.setText(name);
+        textoAnterior.setText(text);
         
     }
 }
